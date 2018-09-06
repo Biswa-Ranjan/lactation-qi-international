81747bc20a15633a6eadcc85a832c64a7f3acfb1
ratikanta-sdrc <ratikanta@sdrc.co.in>
2018-09-06 07:12:52 +0530

    bf expression date list pre populated

--- src/providers/bf-expression-date-list-service/bf-expression-date-list-service.ts
+++ src/providers/bf-expression-date-list-service/bf-expression-date-list-service.ts
@@ -1,8 +1,19 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Storage } from '@ionic/storage';
-import { ConstantProvider } from '../constant/constant';
-import { UserServiceProvider } from '../user-service/user-service';
+import {
+  HttpClient
+} from '@angular/common/http';
+import {
+  Injectable
+} from '@angular/core';
+import {
+  Storage
+} from '@ionic/storage';
+import {
+  ConstantProvider
+} from '../constant/constant';
+import {
+  UserServiceProvider
+} from '../user-service/user-service';
+import { DatePipe } from '@angular/common';
 
 /**
  * @author - Subhadarshani
@@ -15,47 +26,29 @@ import { UserServiceProvider } from '../user-service/user-service';
 @Injectable()
 export class BFExpressionDateListProvider {
 
-  constructor(public http: HttpClient,private storage: Storage,
-    private userService: UserServiceProvider) {
-  }
- /**
-   * This method will give us all the dates in string array format of which ExpressionBF we have.
+  constructor(public http: HttpClient, private storage: Storage,
+    private userService: UserServiceProvider, private datePipe: DatePipe) {}
+  /**
+   * This method will give us all the dates in string array from delivery date to current date.
+   * If baby is discharged between delivery date and current date, the string array with contain 
+   * dates from delivery dates to discharge date.
    * @author Subhadarshani
+   * @author Ratikanta
    * @since 0.0.1
    * @returns Promise<string[]> string array of dates
    * @param babyid the patient id for which we are extracting data
    */
-  getExpressionBFDateListData(babyCode: string): Promise<string[]> {
+  async getExpressionBFDateListData(babyCode: string) {
 
-    let promise : Promise<string[]>= new Promise((resolve, reject) => {
-      this.storage.get(ConstantProvider.dbKeyNames.bfExpressions)
-      .then(data=>{
-        if(data != null){
+    try {
+      let patient: IPatient = (await this.storage.get(ConstantProvider.dbKeyNames.patients) as IPatient[])
+                                .filter(d => d.babyCode === babyCode)[0]  
+                                                              
+      return this.getDateList(patient.deliveryDate, patient.dischargeDate)
 
-          data = (data as IBFExpression[]).filter(d=> d.babyCode === babyCode)
-
-          //Checking if there is any data belong to the patient id or not
-          if((data as IBFExpression[]).length > 0){
-            let dates:string[] = [];
-            (data as IBFExpression[]).forEach(d => {
-              dates.push(d.dateOfExpression)
-            });
-            //removing duplicates
-            dates = Array.from(new Set(dates))
-
-            resolve(dates)
-          }else{
-            resolve([]);
-          }
-        }else{
-          resolve([]);
-        }
-      })
-      .catch(err=>{
-        reject(err.message);
-      })
-    });
-    return promise;
+    } catch (err) {
+      return err.message
+    }
   }
 
   /**
@@ -65,27 +58,27 @@ export class BFExpressionDateListProvider {
    * @returns Promise<any[]>  array of dates
    * @param babyid the patient id for which we are extracting data
    */
-  findByBabyCodeAndDate(babyCode: string, date: string, isNewExpression: boolean): Promise<IBFExpression[]>{
+  findByBabyCodeAndDate(babyCode: string, date: string, isNewExpression: boolean): Promise < IBFExpression[] > {
 
-    let promise: Promise<IBFExpression[]> = new Promise((resolve, reject)=>{
-      if(date !== null){
+    let promise: Promise < IBFExpression[] > = new Promise((resolve, reject) => {
+      if (date !== null) {
         this.storage.get(ConstantProvider.dbKeyNames.bfExpressions)
-        .then(data=>{
-          if(data != null){
-            data = (data as IBFExpression[]).filter(d => d.babyCode === babyCode && d.dateOfExpression === date);
-            if((data as IBFExpression[]).length > 0){
-              resolve(this.defaultDisplayOfEntries(data, ConstantProvider.noOfRecordsByDefault - data.length, babyCode, date))
-            }else{
-              resolve(this.defaultDisplayOfEntries(data, ConstantProvider.noOfRecordsByDefault, babyCode, date))
+          .then(data => {
+            if (data != null) {
+              data = (data as IBFExpression[]).filter(d => d.babyCode === babyCode && d.dateOfExpression === date);
+              if ((data as IBFExpression[]).length > 0) {
+                resolve(this.defaultDisplayOfEntries(data, ConstantProvider.noOfRecordsByDefault - data.length, babyCode, date))
+              } else {
+                resolve(this.defaultDisplayOfEntries(data, ConstantProvider.noOfRecordsByDefault, babyCode, date))
+              }
+            } else {
+              resolve(this.defaultDisplayOfEntries([], ConstantProvider.noOfRecordsByDefault, babyCode, date))
             }
-          }else{
-            resolve(this.defaultDisplayOfEntries([], ConstantProvider.noOfRecordsByDefault, babyCode, date))
-          }
-        })
-        .catch(err=>{
-          reject(err.message)
-        })
-      }else{
+          })
+          .catch(err => {
+            reject(err.message)
+          })
+      } else {
         resolve(this.defaultDisplayOfEntries([], ConstantProvider.noOfRecordsByDefault, babyCode, date))
       }
     });
@@ -93,16 +86,16 @@ export class BFExpressionDateListProvider {
   }
 
   /**
- * This method is going to append a new BfExpression object to existing list
- *
- * @param {IBFExpression[]} data The existing list
- * @param {string} babyCode The unique baby code
- * @param {date} The date of feeding
- * @returns {IBFExpression[]} The final appended list
- * @memberof ExpressionBfDateProvider
- */
-  appendNewRecordAndReturn(data: IBFExpression[], babyCode: string, count: number, date?: string): IBFExpression[] {
-    
+   * This method is going to append a new BfExpression object to existing list
+   *
+   * @param {IBFExpression[]} data The existing list
+   * @param {string} babyCode The unique baby code
+   * @param {date} The date of feeding
+   * @returns {IBFExpression[]} The final appended list
+   * @memberof ExpressionBfDateProvider
+   */
+  appendNewRecordAndReturn(data: IBFExpression[], babyCode: string, count: number, date ? : string): IBFExpression[] {
+
     //The blank feed object
 
     // if(data != null){
@@ -112,9 +105,9 @@ export class BFExpressionDateListProvider {
     //   data.push(bf)
     // }
 
-    if(data === null) {
+    if (data === null) {
       data = []
-    }else {
+    } else {
       for (let index = 0; index < count; index++) {
         data.push(this.getNewBfExpressionEntry(babyCode, date))
       }
@@ -150,5 +143,36 @@ export class BFExpressionDateListProvider {
       bfExpressions.push(this.getNewBfExpressionEntry(babyCode, date))
     }
     return bfExpressions
+  }
+
+
+
+  /**
+   * This method is going to return array of dates(dd-MM-yyyy)
+   * @author Ratikanta
+   * @param {Date} deliveryDate Delivery date of the patient
+   * @param {Date} dischargeDate Discharge date of the patient
+   * @returns {string[]} Array of dates 
+   * @memberof BFExpressionDateListProvider
+   */
+  getDateList(deliveryDate: string, dischargeDate: string): string[] {
+
+    let dateList: string[] = []
+    let today: string = this.datePipe.transform(new Date(), 'dd-MM-yyyy')
+    let startDate: any = new Date(deliveryDate.split('-')[1] + '-' + deliveryDate.split('-')[0] + '-' +deliveryDate.split('-')[2])
+    let endDate: any = dischargeDate != null? new Date(dischargeDate.split('-')[1] + '-' + dischargeDate.split('-')[0] + '-' +dischargeDate.split('-')[2]) : new Date(today.split('-')[1] + '-' + today.split('-')[0] + '-' +today.split('-')[2])
+
+    let differenceInDays:number = Math.round((endDate-startDate)/(1000*60*60*24))
+
+    dateList.push(deliveryDate)
+    for(let i = 1;i< differenceInDays;i++){
+      let newDate: Date = new Date(deliveryDate.split('-')[1] + '-' + deliveryDate.split('-')[0] + '-' +deliveryDate.split('-')[2])
+      dateList.push(this.datePipe.transform(newDate.setDate(newDate.getDate() + i), 'dd-MM-yyyy')) 
+    }
+
+    if(differenceInDays > 0)
+      dateList.push(this.datePipe.transform(endDate, 'dd-MM-yyyy'))    
+    
+    return dateList
   }
 }
